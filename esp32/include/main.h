f5100250c64662488feca7389923db77caae2472
Ryan Cheng <ryan.zy.cheng@gmail.com>
2025-07-21 15:43:46 -0700

    edit: limit switch pin

--- esp32/include/main.h
+++ esp32/include/main.h
@@ -1,12 +1,7 @@
 #pragma once
 #include <Arduino.h>
 
-// pin definitions
-#define M1_PIN_FWD         20
-#define M1_PIN_REV         21
-#define M2_PIN_FWD         19
-#define M2_PIN_REV         22
-
+// arm pins
 #define SERVO_BASE_PIN     14
 #define SERVO_SHOULDER_L   12
 #define SERVO_SHOULDER_R   13
@@ -14,8 +9,25 @@
 #define SERVO_WRIST_PIN    27
 #define SERVO_CLAW_PIN     15
 
-#define RX1_PIN            7
-#define TX1_PIN            8
+// limit switch
+#define LIMIT_SWITCH_PIN    7
+
+// motor pins
+#define M1_PIN_FWD         20
+#define M1_CHAN_FWD         0
+#define M1_PIN_REV         21
+#define M1_CHAN_REV         1
+
+#define M2_PIN_FWD         19
+#define M2_CHAN_FWD         2
+#define M2_PIN_REV         22
+#define M2_CHAN_REV         3
+
+// reflectance sensor pins
+#define ANALOG_PIN_R1 37  // GPIO37 = ADC1_CHANNEL_1
+#define ANALOG_PIN_L1 38  // GPIO38 = ADC1_CHANNEL_2
+#define ANALOG_PIN_R2 34  // GPIO34 = ADC1_CHANNEL_6
+#define ANALOG_PIN_L2 35  // GPIO35 = ADC1_CHANNEL_7
 
 // pwm settings
 #define PWM_FREQ           1000
