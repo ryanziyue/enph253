a1a5b8131f776bf9616fe845f29293ce6a7e7e8a
Ryan Cheng <ryan.zy.cheng@gmail.com>
2025-07-31 12:28:39 -0700

    fix: wrist servo lock angle

--- esp32/include/main.h
+++ esp32/include/main.h
@@ -70,7 +70,7 @@
 #define SHOULDER_R_OFFSET       174.0 
 #define WRIST_LOWER_LIMIT       -75
 #define WRIST_UPPER_LIMIT       105
-#define WRIST_SERVO_ALIGNED     75 - 22      // servo angle when aligned with second arm
+#define WRIST_SERVO_ALIGNED     75 + 22      // servo angle when aligned with second arm
 #define BASE_SERVO_OFFSET       10    // base servo offset from 0deg
 
 
